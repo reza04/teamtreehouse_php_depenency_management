94ccd88c0316560dcd78b33f561f52d9186b5c57
Reza Aditya Pratama_31 <rezaaditya9132@gmail.com>
2020-10-20 10:18:49 +0700

    message

--- phpmailer/get_auth_token.php
+++ phpmailer/get_auth_token.php
@@ -75,8 +75,8 @@ if (!in_array($providerName, ['Google', 'Microsoft', 'Yahoo'])) {
 
 //These details are obtained by setting up an app in the Google developer console,
 //or whichever provider you're using.
-$clientId = '287061062864-spo5gj601scccoarjh1t7novm01rhogr.apps.googleusercontent.com';
-$clientSecret = '_5KyWWwzppZTZwpnmNVcNiiZ';
+// $clientId = '287061062864-spo5gj601scccoarjh1t7novm01rhogr.apps.googleusercontent.com';
+// $clientSecret = '_5KyWWwzppZTZwpnmNVcNiiZ';
 
 //If this automatic URL doesn't work, set it yourself manually to the URL of this script
 $redirectUri = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
